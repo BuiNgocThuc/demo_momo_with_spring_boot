e078663715c9a201c8f65ef298842131a07ba9b5
Thuc Ngoc Bui <thucngb.dev@gmail.com>
2024-10-16 16:57:49 +0700

    initial project

---
+++ src/main/java/com/example/demomomo/DemoMomoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMomoApplication {
+
+        public static void main(String[] args) {
+                SpringApplication.run(DemoMomoApplication.class, args);
+        }
+
+}
---
+++ src/test/java/com/example/demomomo/DemoMomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMomoApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+
+}
