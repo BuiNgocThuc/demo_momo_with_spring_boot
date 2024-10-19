01fefb02088afeba416370318fa3c7c5c252b081
PhatJack <tienphatng.693@gmail.com>
2024-10-19 09:39:41 +0700

    refactor: Remove console.log and unused code

--- client/src/features/api/apiSlice.ts
+++ client/src/features/api/apiSlice.ts
@@ -2,9 +2,13 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_SERVER_URL }),
-	tagTypes: ['Auth','Service','Bill','Report','Survey'],
-	endpoints: (builder) => ({
-
-	})
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_SERVER_URL,
+    prepareHeaders: (headers) => {
+      headers.set('ngrok-skip-browser-warning', '69420')
+      return headers
+    },
+  }),
+  tagTypes: ['Auth', 'Service', 'Bill', 'Report', 'Survey'],
+  endpoints: (builder) => ({}),
 })
